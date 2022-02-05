d0a371936a54007a23f03c1a58a52c67d51cb527
ryuxing <31610107+ryuxing@users.noreply.github.com>
2022-02-06 02:06:13 +0900

    Memberから入室時にオブジェクト生成可能に

--- script.js
+++ script.js
@@ -1,73 +1,133 @@
-
 var Room=null;
-var Stream = null;
+var localStream = null;
+var stream;
+const NO_STREAM = getEmptyStream();
+authInfo = null;
 window.onload = async()=>{
-    Stream = await navigator.mediaDevices
+    localStream = await navigator.mediaDevices
         .getUserMedia({
             audio : true,
             video : true
         });
     const init_local_video = document.getElementById("init-local-video");
     init_local_video.muted = true;
-    init_local_video.srcObject = Stream;
+    init_local_video.srcObject = localStream;
     init_local_video.playsInline = true;
-    await init_local_video.play(); 
-    document.getElementById("join").addEventListener("click",()=>{
-        if(Room == null){
-            Room = new ROOM("test",Stream);
-        }
-    })
+    await init_local_video.play();
+    stream = localStream;
+    authInfo = FirebaseAuth.currentUser;
+    initAccountStatus("",true);
+    document.getElementById("account").addEventListener("click",initAccountStatus);
 }
-const peer = (window.peer = new Peer({
-key: window.__SKYWAY_KEY__,
-debug: 3,
-}));
+function getEmptyStream(){
+    let canvas = document.createElement("canvas");
+    canvas.width = 1;
+    canvas.height = 1;
+    let ctx = canvas.getContext("2d");
+    ctx.fillStyle = "green";
+    ctx.fillRect(0,0,1,1);
+    let vstream = canvas.captureStream(1);
+    var AudioContext = window.AudioContext || window.webkitAudioContext;
+    let audio = new AudioContext();
+    let astream = audio.createMediaStreamDestination().stream;
 
+    return new MediaStream([vstream.getTracks()[0],astream.getTracks()[0]]);
+}
+async function initAccountStatus(elem, onlyDisplay){
+    if(FirebaseAuth.currentUser==null){
+        if(!onlyDisplay)authInfo = await FirebaseSignIn();
+        if(authInfo ==null){return;}
+        document.querySelector("#account div").style.backgroundImage = `url("${authInfo.photoURL}")`;
+        document.querySelector("#account span").innerText = authInfo.name;
+        document.getElementById("join").addEventListener("click",joinRoom);
+        let digits = 36 ** 5;
+        peerId = authInfo.uid + "-"+ (new Date().getTime()).toString(36)+ (Math.floor(Math.random())*digits).toString(36);
+        console.log(peerId);
+        window.peer = new Peer(peerId,{key: window.__SKYWAY_KEY__,debug:1});
+        console.log(peer);
+    
+    }else{
+        await FirebaseSignOut(FirebaseAuth);
+        document.querySelector("#account div").style.backgroundImage = `url("${NO_IMAGE}")`;
+        document.querySelector("#account span").innerText = "ログイン";
+        document.getElementById("join").removeEventListener("click",joinRoom);
+        if(window.peer != undefined){
+            peer.destroy();
+            delete peer;
+        }
+        authInfo = null;
+    }
+}
+function joinRoom(){
+    var RoomId = "test";
+    if(authInfo==null){return false;}
+    window.Room = new ROOM(RoomId,NO_STREAM);
+    document.getElementById("join").removeEventListener("click",joinRoom);
+
+}
 class ML{
     static list ={};
     static get memberList(){
-        
+        return Object.keys(this.list);
     }
-    static addMember(peerId){
-
+    static get l(){ return this.list};
+    static addMember(uid){
+        if(uid in this.list){
+            return false;
+        }
+        this.list[uid] = new Member(uid);
+        for(let mem of this.memberList){
+            //mem.addCanvas()
+            console.log(mem);
+        }
+        return true;
     }
-    static removeMember(peerId){
-
+    static removeMember(uid){
+        //canvasのIDにdisabledを追加
+        //elementを削除
+        //memberをdelete
+        //remove from member list
     }
 }
-const NO_IMAGE = "image/noimage.png";
+const NO_IMAGE = "/img/noimage.png";
 class Member{
-    Stream = null;
+    currentStream = null;
+    peers = [];
+    streams ={};
     isStream = false;
-    peerId = "";
     name   = "No Name";
+    uid = "";
     icon   = NO_IMAGE;
     color  = "gray";
     pointer=null;
     video  = null;
     canvas = {};
-    element= null;
+    element= {};
     aspectRatio=1;
-    constructor(peerId){
-        this.peerId = peerId;
+    constructor(uid){
+        this.uid = uid;
         //Pointer
         this.pointer = document.createElement("div");
         this.pointer.style.backgroundColor=this.color;
         this.pointer.classList.add("pointer");
         this.pointer.classList.add("hidden");
+        this.pointer.dataset.uid = this.uid;
         document.getElementById("pointers").appendChild(this.pointer);
-    }
-    async initVideo(stream){
-        this.isStream = true;
+
         //Video
-        this.stream = stream;
         this.element.video  = document.createElement("video");
+        this.element.video.dataset.uid = this.uid;
         this.element.video.muted = true;
         this.element.video.srcObject=stream;
         this.element.video.playsInline = true;
+        
+
         //Div
         this.element.div    = document.createElement("div");
         this.element.div.classList.add("member");
+        this.element.div.classList.add("hidden");
+        this.element.div.dataset.uid = this.uid;
+
         //Footer
         this.element.footer = document.createElement("div");
         this.element.footer.style.backgroundColor = this.color;
@@ -79,6 +139,20 @@ class Member{
         //Merge
         this.element.div.appendChild(this.element.video);
         this.element.div.appendChild(this.element.footer);
+        this.video = this.element.video;
+        document.getElementById("stream").appendChild(this.element.div);
+    }
+    addPeer(peerId){
+        this.peers.push(peerId);
+    }
+    removePeer(peerId){
+        this.peers.splice(this.peers.indexOf(peerId),1);
+        delete this.stream[peerId];
+    }
+    async initVideo(stream){
+        console.log(stream);
+        this.video.srcObject = stream;
+        this.currentStream = stream;
         await this.video.play();
         //Videoのオブザーバ追加
 
@@ -128,7 +202,7 @@ class ROOM{
     room = null;
     constructor(roomId,stream){
         try{
-            this.room = peer.joinRoom(roomId, {
+            this.room = window.peer.joinRoom(roomId, {
                 mode: "sfu",
                 stream: stream
             });    
@@ -141,7 +215,6 @@ class ROOM{
         this.room.on('stream',this.onStream);
         this.room.on('data',this.onData);
         this.room.on('peerLeave',this.onMemberLeave);
-        this.room.on('stream',this.onStream);
         this.room.once('open',this.onceJoin);
         this.room.once('close',this.onceLeave);        
         return this;
@@ -150,17 +223,11 @@ class ROOM{
         //peerIdからメンバにいない人の収集
         //Streamのpeer-idからビデオをバインドする
         //await ML.list[stream.peerId].initVideo(stream);
+        let uid = stream.peerId.split("-")[0];
+        console.log(uid);
+        ML.addMember(uid);
+        ML.l[uid].initVideo(stream);
 
-        //Test
-        console.log(stream.peerId);
-        var vi = document.createElement("video");
-        vi.style.width="200px";
-        vi.style.height="auto";
-        vi.srcObject = stream;
-        vi.dataset.id= stream.peerId;
-        console.log(stream);
-        document.body.appendChild(vi);
-        await vi.play();
     }
     async onData({data,src}){
         var txt = document.createElement("span");
